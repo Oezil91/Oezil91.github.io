e58dd28a5331cd7809abc27be62bde8ea7fb796a
Oezil91 <liverpool.rahman91@gmail.com>
2022-01-19 10:20:54 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// dengan vanila javascript
+//   document.addEventListener("DOMContentLoaded", function () {
+//     var elems = document.querySelectorAll(".sidenav");
+//     M.Sidenav.init(elems);
+//   });
+
+//   dengan jquery
+//   $(document).ready(function () {
+//     $(".sidenav").sidenav();
+//   });
+
+// dengan DOM
+const sideNav = document.querySelectorAll(".sidenav");
+M.Sidenav.init(sideNav);
+
+const slider = document.querySelectorAll(".slider");
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  duration: 600,
+  interval: 3000,
+});
+
+const parallax = document.querySelectorAll(".parallax");
+M.Parallax.init(parallax);
+
+const materialbox = document.querySelectorAll(".materialboxed");
+M.Materialbox.init(materialbox);
+
+const scroll = document.querySelectorAll(".scrollspy");
+M.ScrollSpy.init(scroll, {
+  scrollOffset: 40,
+});
